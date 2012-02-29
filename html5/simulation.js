3563739801effa77a519d055e51ce17e7f5105ac
Stanford Rosenthal <nolastan@gmail.com>
2012-02-28 18:41:10 -0600

    added person vector; reorganized objects in simulation

--- html5/simulation.js
+++ html5/simulation.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
   // setup the canvas
   var CANVAS_WIDTH = 480;
-  var CANVAS_HEIGHT = 320;
+  var CANVAS_HEIGHT = 500;
   var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
                         "' height='" + CANVAS_HEIGHT + "'></canvas>");
   var canvas = canvasElement.get(0).getContext("2d");
@@ -16,12 +16,34 @@ $(document).ready(function(){
   // game variables
   var partygoers = new Array();
   var timeInDay = 300;
- 
-  // images
-  var partyGoerImg = new Image();
-  partyGoerImg.src = "sprites/player.png";
-  var houseImg = new Image();
-  houseImg.src = "sprites/imgres.jpg";
+  var partygoerLife = 75;
+  
+  // objects
+  function Person(){
+  }
+  function House(){
+    var img = new Image();
+    img.src = "sprites/imgres.jpg";
+  }
+  Partygoer.prototype = new Person();
+  function Partygoer(){
+    this.draw = function(x, y){
+      drawPerson(canvas, x, y, 1, "blue")
+    }
+  }
+  Rushee.prototype = new Person();
+  function Rushee(){
+    this.name = "bob";
+    this.draw = function(x, y){
+      drawPerson(canvas, x, y, 1, "red")
+    }
+  }
+  Philanthropist.prototype = new Person();
+  function Philanthropist(){
+    this.draw = function(x, y){
+      drawPerson(canvas, x, y, 1, "green")
+    }
+  }
   
   // Simulate!
   function simulate(rep){
@@ -31,61 +53,30 @@ $(document).ready(function(){
     var intervalLength = timeInDay / rep;
     var interval = 0;
     
-    var FPS = 30;
-    setInterval(function() {
-      
-      if(interval > timeInDay){
-        stop();
-      }
-      
-      // ---- Update ---- \\
-      if(interval % intervalLength == 0){
-        partygoers.push(partyGoer());
-      }
-      for(i = 0; i < partygoers.length; i++){
-        partygoers[i].x += i;
-      }
-        
-      // ---- Draw ---- \\
-      canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    rushee = new Rushee();
+    rushee.draw(10,10);
 
-      // draw new frame
-      house.draw();
-      for(i = 0; i < partygoers.length; i++){
-        partygoers[i].draw();
-      }
-      
-      interval++;
-      
-    }, 1000/FPS);
-  }
-  
-  
-  // game objects
-  function partyGoer(I){
-    I = I || {}
-    I.sprite = partyGoerImg;
-    I.x = 10;
-    I.y = 10;
-    I.draw = function(){
-      canvas.drawImage(this.sprite, this.x, this.y);      
-    }
-    return I;
-  }
     
-  var house = {
-    color: "#00A",
-    x: 120,
-    y: 10,
-    width: 32,
-    height: 32,
-    sprite: houseImg,
-    draw: function() {
-      canvas.fillStyle = this.color;
-      canvas.drawImage(this.sprite, this.x, this.y);
-    }
-  };  
-  
+    
+    // var FPS = 30;
+    // setInterval(function() {
+    //   
+    //   if(interval > timeInDay){
+    //     stop();
+    //   }
+    //   
+    //   // ---- Update ---- \\
+    // 
+    //   // ---- Draw ---- \\
+    //   canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // 
+    //   // draw new frame
+    //   drawPerson(canvas, 10, 10, 4)
+    //   
+    //   interval++;
+    //   
+    // }, 1000/FPS);
+  }  
 
 });
 
