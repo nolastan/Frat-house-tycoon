31ce6440af5bd39977bd3aca205453d8eb57bf14
Dennis  Tyberend <dtyberend@gmail.com>
2012-03-21 22:43:05 -0500

    Added board2.js for modification of the Board class. Added playing_piece.js for addition of the PlayingPiece class. Modified new_piece.htm to test out the new Board and PlayingPiece classes.

---
+++ html5/board2.js
@@ -0,0 +1,99 @@
+
+//********************************************************************************************			
+// Globals
+//********************************************************************************************
+var Board = function() {
+
+var   partyRect, studyRect, csRect, rushRect;
+var   stage;
+var   piecesArray = new Array();
+var   first_piece_x, first_piece_y;
+
+//********************************************************************************************
+
+this.addPiece = function (new_member)
+{
+   var piece = new PlayingPiece(stage, new_member);  
+   piece.drawPiece(first_piece_x, first_piece_y);
+   piecesArray.push(piece);
+   
+}   
+
+//********************************************************************************************	
+
+this.drawBoard = function() 
+{
+	//numBrothers = game.frat.members;
+	//updateScreenSize();
+	
+	//console.log("height"+CANVAS_HEIGHT);
+	//console.log("width"+CANVAS_WIDTH);
+	
+	//stage = new Kinetic.Stage("container", CANVAS_WIDTH*0.98, CANVAS_HEIGHT-100);
+	stage = new Kinetic.Stage("container", 1000, 500);
+	var boardLayer   = new Kinetic.Layer();
+	var messageLayer = new Kinetic.Layer();				
+
+	// Make the board layers
+    partyRect = new Kinetic.Rect({
+		x: 10,
+		y: 10,
+		width: stage.width*0.35,
+		height: stage.height*0.35,
+		//fill: "#00D2FF",
+		stroke: "black",
+		strokeWidth: 2
+	});				
+	boardLayer.add(partyRect);
+	
+	csRect = new Kinetic.Rect({
+		x: partyRect.x + partyRect.width,
+		y: 10,
+		width: stage.width*0.35,
+		height: stage.height*0.35,
+		//fill: "#00D2FF",
+		stroke: "black",
+		strokeWidth: 2
+	});				
+	boardLayer.add(csRect);	
+	
+	first_piece_x = csRect.x + csRect.width + 10;
+	first_piece_y = 10;
+	
+	rushRect = new Kinetic.Rect({
+		x: 10,
+		y: partyRect.y + partyRect.height,
+		width: stage.width*0.35,
+		height: stage.height*0.35,
+		//fill: "#00D2FF",
+		stroke: "black",
+		strokeWidth: 2
+	});				
+	boardLayer.add(rushRect);	
+	
+	studyRect = new Kinetic.Rect({
+		x: partyRect.x + partyRect.width,
+		y: partyRect.y + partyRect.height,
+		width: stage.width*0.35,
+		height: stage.height*0.35,
+		//fill: "#00D2FF",
+		stroke: "black",
+		strokeWidth: 2
+	});				
+	boardLayer.add(studyRect);	
+
+	stage.add(boardLayer);	
+
+	// Draw the text inside of the rectangles
+	stage.add(messageLayer);
+	var context = messageLayer.getContext();
+	context.font = "bold 18pt Calibri";
+	context.fillStyle = "black";
+	context.textAlign = "center";
+	context.fillText("Party", partyRect.x + partyRect.width / 2, partyRect.y + partyRect.height / 2);
+	context.fillText("Community Services", csRect.x + csRect.width / 2, csRect.y + csRect.height / 2);
+	context.fillText("Rush", rushRect.x + rushRect.width / 2, rushRect.y + rushRect.height / 2);
+	context.fillText("Study", studyRect.x + studyRect.width / 2, studyRect.y + studyRect.height / 2);
+
+}
+}
---
+++ html5/playing_piece.js
@@ -0,0 +1,109 @@
+var PlayingPiece = function(stage, member) 
+{
+	this.stage = stage;
+	this.member = member;
+
+	this.drawPiece = function (xPos, yPos)
+	{
+		var shapesLayer = new Kinetic.Layer();
+		var group = new Kinetic.Group({
+			draggable: true
+		});                
+			
+		var boxHeader = new Kinetic.Rect({
+			x: xPos,
+			y: yPos,
+			width: 90,
+			height: 15,					
+			fill: "#00CCFF",
+			stroke: "black",
+			strokeWidth: 1
+		});
+		
+		var headerText = new Kinetic.Text(
+		{
+			x: boxHeader.x + 2,
+			y: boxHeader.y + 2,
+			text: "Brian",
+			fontSize: 10,
+			fontFamily: "Calibri",
+			textFill: "black"                    
+		});
+		
+		var boxMain = new Kinetic.Rect({
+			x: boxHeader.x,
+			y: boxHeader.y + boxHeader.height,
+			width: boxHeader.width,
+			height: 50,					
+			fill: "#99FFFF",
+			stroke: "black",
+			strokeWidth: 1
+		});
+		
+		var mainText1 = new Kinetic.Text(
+		{
+			x: boxMain.x + 2,
+			y: boxMain.y + 2,
+			text: "Party Skill: 2.0",
+			fontSize: 10,
+			fontFamily: "Calibri",
+			textFill: "black"                    
+		});
+		
+		var mainText2 = new Kinetic.Text(
+		{
+			x: mainText1.x,
+			y: mainText1.y + 12,
+			text: "Rush Skill: 3.1",
+			fontSize: 10,
+			fontFamily: "Calibri",
+			textFill: "black"                    
+		});
+		
+		var mainText3 = new Kinetic.Text(
+		{
+			x: mainText2.x,
+			y: mainText2.y + 12,
+			text: "CS Skill: 2.2",
+			fontSize: 10,
+			fontFamily: "Calibri",
+			textFill: "black"                    
+		});
+		
+		var mainText4 = new Kinetic.Text(
+		{
+			x: mainText3.x,
+			y: mainText3.y + 12,
+			text: "Study Skill: 3.7",
+			fontSize: 10,
+			fontFamily: "Calibri",
+			textFill: "black"                    
+		});
+		
+		boxHeader.on("mouseover", function(){
+			document.body.style.cursor = "pointer";
+		});
+		boxHeader.on("mouseout", function(){
+			document.body.style.cursor = "default";
+		});
+		
+		boxMain.on("mouseover", function(){
+			document.body.style.cursor = "pointer";
+		});
+		boxMain.on("mouseout", function(){
+			document.body.style.cursor = "default";
+		});
+		
+		group.add(boxHeader);
+		group.add(headerText);
+		group.add(boxMain);
+		group.add(mainText1);   
+		group.add(mainText2); 
+		group.add(mainText3);   
+		group.add(mainText4);  				
+		
+		
+		shapesLayer.add(group);
+		stage.add(shapesLayer);
+	}
+}
